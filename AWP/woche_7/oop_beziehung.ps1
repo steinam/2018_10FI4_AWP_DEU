c8c75643b690dea111777a8f39e5b4b61da1ed84
steinam <webmaster@steinam.de>
2019-05-07 15:46:55 +0200

    OOP_Aufgabe_Mitarbeiter_Auto

---
+++ AWP/woche_7/oop_beziehung.ps1
@@ -0,0 +1,52 @@
+﻿class Auto{
+    [string]$Hersteller
+    [string]$Modell
+    [string]$Leistung
+    [string]$Verbrauch
+    [string]$Baujahr
+    [int]$KilometerStand
+    [string]$Kennzeichen
+
+    [bool]$DoorClosed
+
+
+    Auto([bool]$doorstate)
+    {
+        $this.DoorClosed = $doorstate
+    }
+
+
+    fahren($distanz)
+    {
+        if($this.DoorClosed -eq $true)
+        {
+            $this.KilometerStand += $distanz
+        }
+    }
+}
+
+
+class Mitarbeiter{
+
+    [string]$Name
+    [Auto]$myCar
+
+
+    [void]geheAufReise($ort, $km)
+    {
+        $this.myCar.fahren($km)
+    }
+}
+
+
+$car = [Auto]::new($true)
+$car.fahren(234)
+
+$Brenner = [Mitarbeiter]::new()
+$Brenner.Name = "Sebastian Brenner"
+$Brenner.myCar = $car
+
+$Brenner.geheAufReise("Hamburg", 700)
+
+$car
+
